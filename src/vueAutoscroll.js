206bce4f133b45d0e591e767c4117f9c9b73b5ad
donmbelembe <donmbelembe@gmail.com>
2017-09-08 02:46:40 +0530

    export autoscroll objet so that we can include it directly from a component without installing globally

--- src/vueAutoscroll.js
+++ src/vueAutoscroll.js
@@ -1,19 +1,21 @@
 // import autoscroll from './directives/autoscroll';
 
+export const autoscroll = {
+  bind: function (el, binding, vnode) {
+    var s = JSON.stringify
+    el.innerHTML =
+      'name: ' + s(binding.name) + '<br>' +
+      'value: ' + s(binding.value) + '<br>' +
+      'expression: ' + s(binding.expression) + '<br>' +
+      'argument: ' + s(binding.arg) + '<br>' +
+      'modifiers: ' + s(binding.modifiers) + '<br>' +
+      'vnode keys: ' + Object.keys(vnode).join(', ')
+  }
+}
+
 const VueAutoscroll = {
   install (Vue, options) {
-    Vue.directive('demo', {
-      bind: function (el, binding, vnode) {
-        var s = JSON.stringify
-        el.innerHTML =
-          'name: ' + s(binding.name) + '<br>' +
-          'value: ' + s(binding.value) + '<br>' +
-          'expression: ' + s(binding.expression) + '<br>' +
-          'argument: ' + s(binding.arg) + '<br>' +
-          'modifiers: ' + s(binding.modifiers) + '<br>' +
-          'vnode keys: ' + Object.keys(vnode).join(', ')
-      }
-    })
+    Vue.directive('demo', autoscroll)
   }
 }
 
