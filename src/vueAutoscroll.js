73ffe0f53629eea58e483f2d0f7f6c1e18b3f4c3
donmbelembe <donmbelembe@gmail.com>
2017-09-07 13:46:04 +0530

    import ./directives/autoscroll directive and distribute  it as a vue js plugin

--- src/vueAutoscroll.js
+++ src/vueAutoscroll.js
@@ -1,1 +1,9 @@
-console.log('name changed')
+import autoscroll from './directives/autoscroll.js';
+
+const vueAutoscroll = {
+  install(Vue, options) {
+    Vue.directive(autoscroll.name, autoscroll)
+  }
+};
+
+export default vueAutoscroll;
