91e61dba2d7ace3bf38b42fa72a781237b8eb4b7
donmbelembe <donmbelembe@gmail.com>
2017-09-08 01:57:41 +0530

    update main entry file, set vue as external dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path')
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const config = require('./package.json')
+// const config = require('./package.json')
 
 const webpack = require('webpack')
 
 // Config object
 const library = {
   name: 'VueAutoscroll',
-  target: 'umd'
+  target: 'umd',
+  entry: 'src/vueAutoscroll.js'
 }
 
 const DEV = process.env.NODE_ENV === 'development'
 const WATCH = process.env.NODE_ENV === 'watch'
+const PROD = process.env.NODE_ENV === 'production'
 
 let webpackConfig = {
-  entry: path.resolve(__dirname, config.main),
+  entry: path.resolve(__dirname, library.entry),
   watch: WATCH,
   output: {
     library: library.name,
     libraryTarget: library.target,
     path: path.resolve(__dirname, 'dist'),
-    filename: (DEV) ? 'autoscroll.js' : 'autoscroll.min.js',
+    filename: (PROD) ? 'autoscroll.min.js' : 'autoscroll.js',
     publicPath: '/dist/'
   },
-  devtool: DEV ? 'cheap-module-eval-source-map' : 'source-map',
+  externals: {
+    vue: {
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue',
+      root: '_'
+    }
+  },
+  devtool: DEV || WATCH ? 'cheap-module-eval-source-map' : 'source-map',
   devServer: {
     overlay: true,
     contentBase: path.resolve(__dirname)
@@ -46,7 +56,7 @@ let webpackConfig = {
   plugins: []
 }
 
-if (!DEV && !WATCH) {
+if (PROD) {
   webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
     sourceMap: true,
     compress: { warnings: false }
